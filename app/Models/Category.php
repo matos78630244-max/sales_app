5d0b2ba5981553c605503b52f32ce1541bc8df5a
matos78630244 <matos78630244@gmail.com>
2026-05-09 23:32:50 -0400

    feat: modelos eloquent con relaciones y accessors

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    /**hola */
     use HasFactory;
     protected $fillable = ['name', 'slug', 'description'];
 
